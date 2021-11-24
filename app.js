8b12fe1a9b1a02417247bdd3bdc67d9bd247dfec
shafi <khanshafi424@gmail.com>
2021-11-24 12:12:47 +0530

    implemented the review

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('./database/mongoose');
 
-
-
-
-
-
-
-
 app.listen(3000, ()=>{
     console.log("Server is listning 3000  port Number");
 })
--- database/mongoose.js
+++ database/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect(' mongodb://127.0.0.1:27017/taskmanagerdb')
 .then(()=>{
-    console.log("DB connected successfully!")
+    console.log("DB connected successfully!!!")
 })
 .catch((error)=>{
     console.log("DB connection Failed" ,error)
